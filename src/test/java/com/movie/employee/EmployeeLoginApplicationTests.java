3950a5e0ab715722335cda81b595393835dad0ff
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2022-03-27 21:46:52 -0500

    Spring test confugured - Starting level

--- src/test/java/com/movie/employee/EmployeeLoginApplicationTests.java
+++ src/test/java/com/movie/employee/EmployeeLoginApplicationTests.java
@@ -3,8 +3,11 @@ package com.movie.employee;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest
+@SpringBootTest(classes = {EmployeeLoginApplication.class, H2JpaConfig.class})
+@ActiveProfiles("test")
 class EmployeeLoginApplicationTests {
 
 	@Test
---
+++ src/test/java/com/movie/employee/H2JpaConfig.java
@@ -0,0 +1,13 @@
+package com.movie.employee;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = { "com.movie.employee.*" })
+@EnableTransactionManagement
+public class H2JpaConfig {
+
+}
