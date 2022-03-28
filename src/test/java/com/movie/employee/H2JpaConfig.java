6d20f93f606eccfe72f56a989f21a48f3097a7e5
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2022-03-27 21:57:46 -0500

    Minor tweaks

--- src/test/java/com/movie/employee/H2JpaConfig.java
+++ src/test/java/com/movie/employee/H2JpaConfig.java
@@ -1,7 +1,6 @@
 package com.movie.employee;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
