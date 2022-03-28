55e3e3f570dc1004f9a750677f86b729d69bde3f
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2022-03-27 21:09:10 -0500

    Simple test enabled

--- src/test/java/com/movie/employee/EmployeeLoginApplicationTests.java
+++ src/test/java/com/movie/employee/EmployeeLoginApplicationTests.java
@@ -1,5 +1,7 @@
 package com.movie.employee;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class EmployeeLoginApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
